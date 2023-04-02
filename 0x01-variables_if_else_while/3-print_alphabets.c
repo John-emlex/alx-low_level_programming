61e4ccc3a88ca67eb7aa5629e8764e9cdd0dd8d5
Johnemlex <peterjohnemlex@gmail.com>
2023-04-02 09:46:28 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
-/*
- * main - c programming
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
  *
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
-		for (i = 0; i < 52; i++)
+		for (i = 0; i <= 52; i++)
 	{
 		putchar(alp[i]);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
