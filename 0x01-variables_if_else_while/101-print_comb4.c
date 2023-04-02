8a7c6b38dbed56118050bf6a4cfcabce89b944d4
Johnemlex <peterjohnemlex@gmail.com>
2023-04-02 13:41:36 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,18 +12,22 @@ int main(void)
 
 	for (n = 0; n < 8; n++)
 	{
+
 		for (m = n + 1; m < 9; m++)
 	{
-		for (l = n + 1; l < 10; l++)
+
+		for (l = n + 1; l <= 9 ; l++)
+	{
+		if ((m != n) != l)
 	{
 		putchar((n % 10) + '0');
 		putchar((m % 10) + '0');
 		putchar((l % 10) + '0');
-
 		if (n == 7 && m == 8 && l == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
+	}
 	}
 	}
 	}
